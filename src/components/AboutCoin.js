608635fb4e7611aa516dbd6b9e9f9bee9fac4e0d
CalArcher <p.calarch@gmail.com>
2022-07-06 13:55:57 -0600

    start adding routes

---
+++ src/components/AboutCoin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AboutCoin() {
+  return (
+    <div>
+      <h1>Hello</h1>
+
+      <span>this is a test</span>
+     
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   document.getElementById('root')
 )
