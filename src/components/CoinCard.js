5321c477f03a5c132c44122e9d2806c9210b34ea
CalArcher <p.calarch@gmail.com>
2022-07-09 21:47:51 -0600

    added chart.js, react-chartjs-2 & started making chart

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -6,14 +6,17 @@ import { BrowserRouter as Router, Routes, Switch, Route, Link, Navigate } from '
 
 
 export default function CoinCard({ name, currentPrice, percentChange, dailyChange, rank }) {
+
+  const stackStyle = {
+    display: 'grid',
+    gridTemplateColumns: '45px 1fr 1fr 1fr 1fr 1fr',
+    gap: '1rem'
+  }
+
   return (
         <Card style={{minWidth: '850px'}}>
           <Card.Body>
-          <Stack class="mb-4" direction="horizontal" style={{
-              display: 'grid',
-              gridTemplateColumns: '45px 1fr 1fr 1fr 1fr 1fr',
-              gap: '1rem',
-            }}>
+          <Stack class="mb-4" direction="horizontal" style={stackStyle}>
               <h5 style={{alignContent: 'center', width: '100%'}}className='me-auto'>{rank}</h5>
               <Link to='/about' style={{textDecoration: 'none'}}><h5 className='me-auto'>{name}</h5></Link>
               <span class="me-auto">{currencyFormatter.format(currentPrice)}</span>
--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,12 +1,70 @@
-import React from 'react'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-export default function PriceChart({ name }) {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+//need to define an array here of all days (dates) as the labels, and the data will be an array of the prices on those days
+
+export default function PriceChart( { name } ) {
+
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: name + ' price',
+        data: [1,1,2,3,4.5,9,33,],
+        borderColor: 'rgb(0, 104, 249)',
+        backgroundColor: 'rgba(0, 104, 249, 0.5)',
+      }
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      Tooltip: {
+        // TODO: Figure out how to customize Tooltip
+      },
+      title: {
+        display: true,
+        text: name + ' price history',
+      },
+    },
+  }
+
+  const chartStyle = {
+    width: '1000px',
+    height: '500px',
+  }
+
   return (
-    <div>
-      <div style={{width: '500px', height: '500px', background: 'rgba(220,33,0,.2' }}>
-        <span>{name}</span>
-        <h2>chart here</h2>
-      </div>
+    <div style={chartStyle}>
+      <Line options={options} data={data} />
     </div>
+    
   )
 }
