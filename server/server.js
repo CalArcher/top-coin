59f665b7750420435c791e2f179978bf25f14a35
CalArcher <p.calarch@gmail.com>
2022-07-18 17:46:02 -0600

    added database update and add functionality

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 // import modules / declaring variables
 const express = require('express')
-const axios = require('axios')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const schedule = require('node-schedule')
 const PORT = process.env.PORT 
 const coinDataRoutes = require('./routes/CoinData')
+let updateDb = require('./updateDb')
 
 //app
 const app = express()
@@ -30,7 +30,10 @@ mongoose.connect(process.env.DATABASE_URL)
     console.log('connect success')
     app.listen(PORT, () => {
       console.log('listening on port', PORT)
+      updateDb.compareAndUpdate()
     })    
+
+
   })
   .catch(err => {
     console.log(err)
@@ -44,25 +47,62 @@ mongoose.connect(process.env.DATABASE_URL)
 
 
 
-class GetDbData{
-  constructor(){
-    this.currentCoinData
-  }
-  
-  async fetchCurrent(){
-    let url = 'http://localhost:3000/coindata'
-    try{
-      let data = await axios.get(url)
-      console.log(data)
-      return data
-    } catch(error){
-      console.log(error)
-    }
-  }
-  
-}
 
-let newCoinData = new GetDbData()
 
-newCoinData.fetchCurrent()
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   async function fetchCurrent(){
+//     return ['1', '3', '5']
+//   }
+
+
+//   let testing = []
+//   async function test(){
+//     let a = await fetchCurrent()
+//     a.forEach(e => {
+//       // testing[e] = Number(e) + 1
+//       testing.push(e)
+//     })
+//     return testing
+//   }
+
+//   let tedsting = async () => {
+//     let a = await test()
+//     console.log(a)
+//     return a
+
+//   }
+
+// console.log(tedsting())
 
--- server/updateDb.js
+++ server/updateDb.js
@@ -7,47 +7,128 @@ class UpdateData{
   }
 
   async getNewData(){
-    let newObjs = []
     let url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y'
+    let newObjs = []
     
     try{
       let data = await (await fetch(url)).json()
-
       let top10Today = await data.sort((a,b) => b.price_change_percentage_24h_in_currency - a.price_change_percentage_24h_in_currency).slice(0,10)
       
       for(let i = 0; i<top10Today.length; i++){
         let date = top10Today[i].last_updated.slice(0,10)
+        let changeOneYear = top10Today[i].price_change_percentage_1y_in_currency === null ? -9999 : top10Today[i].price_change_percentage_1y_in_currency
         newObjs.push(
           {
             name: top10Today[i].id,
             rank: top10Today[i].market_cap_rank,
-            current_price: [top10Today[i].current_price],
+            current_price: top10Today[i].current_price,
             percent_change_24h: top10Today[i].price_change_percentage_24h_in_currency,
             percent_change_7d: top10Today[i].price_change_percentage_7d_in_currency,
             percent_change_30d: top10Today[i].price_change_percentage_30d_in_currency,
-            percent_change_1y: top10Today[i].price_change_percentage_1y_in_currency,
+            percent_change_1y: changeOneYear,
             currencyLogo: top10Today[i].image,
-            date: [date]
+            date: date
           }
         )
       }
-      console.log(newObjs)
       return newObjs
     } catch(err){
       console.log(err)
     }
   }
-  async fetchCurrentData(){
-    let url = 'http://10.0.0.69:3000/coindata'
+
+  async fetchCurrent(){
+    let url = 'http://localhost:3000/coindata'
     try{
-      let data = await (await fetch(url)).json()
-      console.log(data.slice(0,1))
-      return data.slice(1,2)
-    } catch(err){
-      console.log(err)
+      let res = await fetch(url)
+      let data = await res.json()
+      return data
+    } catch(error){
+      console.log(error)
     }
   }
 
+  async compareAndUpdate(){
+    let currentData = await this.fetchCurrent()
+    let newData = await this.getNewData()
+   
+    let update = {}
+    let updateId = ''
+    let newCoin = {}
+
+    
+
+    let currentObj = {}
+    for(let i=0; i<currentData.length; i++){
+      let name = currentData[i].name
+      currentObj[name] = currentData[i]
+    }
+    console.log(newData)
+    console.log(currentObj)
+    for(let i=0; i<newData.length; i++){
+      let name = newData[i].name
+  
+      if(currentObj[name]){
+
+        let oldRanks = currentObj[name].rank
+        let newRank = newData[i].rank
+        oldRanks.push(newRank)
+
+        let oldPrices = currentObj[name].current_price
+        let newPrice = newData[i].current_price
+        oldPrices.push(newPrice)
+
+        let oldDates = currentObj[name].date
+        let newDate = newData[i].date
+        oldDates.push(newDate)
+
+        updateId = currentObj[name]._id
+        update = {
+          'rank': oldRanks,
+          'current_price': oldPrices,
+          'percent_change_24h': newData[i].percent_change_24h,
+          'percent_change_7d': newData[i].percent_change_7d,
+          'percent_change_30d': newData[i].percent_change_30d,
+          'percent_change_1y': newData[i].percent_change_1y,
+          date: oldDates
+        }
+        let updateURL = `http://localhost:3000/coindata/${updateId}`
+        try{
+          fetch(updateURL, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(update)
+          }).then(res => {
+            return res.json()
+          })
+        } catch(e) {
+          console.log('line 109')
+          console.log(e)
+        }
+      } else {
+        console.log('POST')
+        let postURL = 'http://localhost:3000/coindata'
+        newCoin = newData[i]
+        console.log(newCoin)
+        try{
+          fetch(postURL, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newCoin)
+          }).then(res => {
+            return res.json()
+          })
+        } catch(e) {
+          console.log(e)
+        }
+      }
+    }
+  }
+ 
 }
 
 
@@ -55,4 +136,5 @@ class UpdateData{
 
 let generateApiData = new UpdateData()
 
-generateApiData.fetchCurrentData()
+
+module.exports = generateApiData
