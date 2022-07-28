362e993c14923f9bf89f407d2a3d4480f4e82113
CalArcher <p.calarch@gmail.com>
2022-07-28 10:48:44 -0600

    clean up

--- client/src/components/TopCoin.js
+++ client/src/components/TopCoin.js
@@ -5,14 +5,12 @@ import { Link } from 'react-router-dom'
 
 export default function TopCoin({ number, name, dailyChange }) {
   return (
-    <Card style={{marginBottom: '2rem'}}>
-      <Card.Title>
+    <div style={{marginBottom: '2rem'}}>
         <div>
           <span style={{
             fontSize: '1.75rem'
           }}>Today's top coin is <Link style={{textDecoration: 'none'}} to={`/about/${name}?q=${number}`} number={number}><span style={{color: '#49D282'}}> { name } </span></Link>, and it's up {percentageFormatter.format(dailyChange)} in the past 24 hours</span>
         </div>
-      </Card.Title>
-    </Card>
+    </div>
   )
 }
