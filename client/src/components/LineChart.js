1878d39f97cba218ea7aa0c73137a1af669b2a1f
CalArcher <p.calarch@gmail.com>
2022-07-30 10:02:30 -0600

    made chart.js chart compatible with dark/light mode

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -2,14 +2,23 @@ import React, { useContext } from 'react'
 import { Context } from '../contexts/DataContext'
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, registerables } from 'chart.js'
-
+import { ThemeContext } from '../contexts/ThemeContextSet'
 ChartJS.register(...registerables)
 
 
 export default function LineChart({ name, number}) {
-  
-  const [{ coinDataSorted, currentNames }, setState] = useContext(Context)
+  const {check, theme, toggleTheme } = useContext(ThemeContext)
+  let bgColor = 'rgba(0, 104, 249, 0.5)'
+  let fgColor = 'rgba(29,29,29,.25)'
+  let chartColor = 'rgb(0,104,249)'
+  let fontColor = 'rgba(29,29,29,1)'
+  if(theme === 'dark'){
+    fontColor = 'rgba(240,240,240,.6)'
+    bgColor = 'rgb(29,29,29)'
+    fgColor = 'rgba(240,240,240,.25)'
+  }
 
+  const [{ coinDataSorted, currentNames }, setState] = useContext(Context)
   console.log("Names",currentNames)
   console.log('coindatasorted',coinDataSorted)
 
@@ -29,17 +38,25 @@ export default function LineChart({ name, number}) {
           pointHoverBorderColor: 'rgba(255, 0, 0, 1)',
           pointRadius: 3,
           data: toChartPrices,
-          borderColor: 'rgb(0, 104, 249)',
-          backgroundColor: 'rgba(0, 104, 249, 0.5)',
+          borderColor: chartColor,
+          backgroundColor: bgColor,
         }
       ],
     }
     const options = {
       scales: {
         x: {
+          grid: {
+            color: fgColor
+          },
           ticks: {
             maxRotation: 0,
             maxTicksLimit: 7
+          }
+        },
+        y: {
+          grid: {
+            color: fgColor
           }
         }
       },
@@ -65,6 +82,7 @@ export default function LineChart({ name, number}) {
         title: {
           display: true,
           text: `${name}'s performance history while ranked in the top 10 coins of the day`,
+          color: fontColor,
           font: {
             weight: 'bold',
             size: '18px'
