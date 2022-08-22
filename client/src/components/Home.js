77a69cd4899e632885560ec8009107b4626e6995
CalArcher <p.calarch@gmail.com>
2022-08-22 09:58:40 -0600

    moved coin table into own component Table.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,10 @@ import { Container } from 'react-bootstrap'
 import CoinCard from './CoinCard'
 import CoinLabels from './CoinLabels'
 import TopCoin from './TopCoin'
+import Table from './Table'
 import NotFound from './NotFound'
 import { Context } from '../contexts/DataContext'
+
 
 function Home() {
   const [{ coinData, currentNames, sortState, setSortState, topCoin }, setState] = useContext(Context)
@@ -14,26 +16,10 @@ function Home() {
     return <NotFound />
   }
 
-  let coinsUpper = currentNames
-
   return (
     <Container className="homeCont">
       <TopCoin number={0} name={topCoin.name} dailyChange={topCoin.percent_change_24h / 100}></TopCoin>
-      <CoinLabels></CoinLabels>
-      {coinData.map((coin, i) => {
-        return (
-          <CoinCard
-            key={i}
-            number={i}
-            name={coinsUpper[i]}
-            currentPrice={coin.current_price[coin.current_price.length - 1]}
-            rank={coin.rank[coin.rank.length - 1]}
-            dailyChange={coin.percent_change_24h / 100}
-            weeklyChange={coin.percent_change_7d / 100}
-            coinLogo={coin.currencyLogo}
-          ></CoinCard>
-        )
-      })}
+      <Table></Table>
     </Container>
   )
 }
---
+++ client/src/components/Table.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import CoinCard from './CoinCard'
+import CoinLabels from './CoinLabels'
+import NotFound from './NotFound'
+import { Context } from '../contexts/DataContext'
+
+function Home() {
+  const [{ coinData, currentNames, sortState, setSortState, topCoin }, setState] = useContext(Context)
+
+  //handles database being empty crashing the site
+  if (!coinData) {
+    return <NotFound />
+  }
+
+  let coinsUpper = currentNames
+
+  return (
+    <div>
+      <CoinLabels></CoinLabels>
+      {coinData.map((coin, i) => {
+        return (
+          <CoinCard
+            key={i}
+            number={i}
+            name={coinsUpper[i]}
+            currentPrice={coin.current_price[coin.current_price.length - 1]}
+            rank={coin.rank[coin.rank.length - 1]}
+            dailyChange={coin.percent_change_24h / 100}
+            weeklyChange={coin.percent_change_7d / 100}
+            coinLogo={coin.currencyLogo}
+          ></CoinCard>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Home;
